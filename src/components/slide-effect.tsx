b2b4ee6840bd237ec16bc144ab9e1607ffc4f290
Ulugbek-desktop <ulugb3k270@gmail.com>
2025-12-16 01:14:52 +0500

    Refactor SlideEffect component for improved readability and consistency in code style

--- src/components/slide-effect.tsx
+++ src/components/slide-effect.tsx
@@ -1,29 +1,49 @@
-'use client'
+"use client";
 
 import { MotionProps, HTMLMotionProps } from "motion/react";
-import * as motion from "motion/react-m"
+import * as motion from "motion/react-m";
 
 interface SlideEffectProps {
   children: React.ReactNode;
-  direction?: 'top' | 'bottom' | 'left' | 'right';
+  direction?: "top" | "bottom" | "left" | "right";
   delay?: number;
   duration?: number;
-  ease?: [number, number, number, number] | 'easeIn' | 'easeOut' | 'easeInOut' | 'linear';
-  isSpring?: boolean
+  ease?:
+    | [number, number, number, number]
+    | "easeIn"
+    | "easeOut"
+    | "easeInOut"
+    | "linear";
+  isSpring?: boolean;
 }
 
-export default function SlideEffect(
-  { children, direction = 'top', delay = 0.1, duration = 0.7, ease = [1, 0, 0, 1], isSpring = true, className }:
-    HTMLMotionProps<'div'> & MotionProps & SlideEffectProps) {
+export default function SlideEffect({
+  children,
+  direction = "top",
+  delay = 0.1,
+  duration = 0.7,
+  ease = [1, 0, 0, 1],
+  isSpring = true,
+  className,
+}: HTMLMotionProps<"div"> & MotionProps & SlideEffectProps) {
   return (
     <motion.div
-      initial={{ opacity: 0, y: direction === 'top' ? 70 : direction === 'bottom' ? -70 : 0, x: direction === 'left' ? 70 : direction === 'right' ? -70 : 0 }}
+      initial={{
+        opacity: 0,
+        y: direction === "top" ? 70 : direction === "bottom" ? -70 : 0,
+        x: direction === "left" ? 70 : direction === "right" ? -70 : 0,
+      }}
       whileInView={{ opacity: 1, y: 0, x: 0 }}
       viewport={{ once: true, amount: 0.2 }}
-      transition={{ duration, ease, delay, type: isSpring ? 'spring' : '' }}
+      transition={{
+        duration,
+        ease,
+        delay,
+        type: isSpring ? "spring" : undefined,
+      }}
       className={className}
     >
       {children}
     </motion.div>
-  )
+  );
 }
