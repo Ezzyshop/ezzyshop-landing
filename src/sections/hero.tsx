84da85eab03db1f5efdbe154b6762a5ce4996045
Ulugbek-desktop <ulugb3k270@gmail.com>
2025-12-16 01:10:36 +0500

    Update package.json to change dev server port, remove yarn.lock, adjust hero section text and styles, and modify global CSS for hero text size.

--- src/sections/hero.tsx
+++ src/sections/hero.tsx
@@ -1,4 +1,4 @@
-'use client'
+"use client";
 
 import FadeEffect from "@/components/fade-effect";
 import Navbar from "@/components/navbar";
@@ -11,17 +11,18 @@ import Link from "next/link";
 import { AnimatedTooltip } from "@/components/ui/animated-tooltip";
 
 const settings = {
-  headline: 'Hello YouTube',
-  subheadline: 'Privacy-friendly web analytics made and hosted in Germany — powerful, cookie-free, and fully compliant with GDPR, CCPA, and PECR.',
+  headline: "Biznesingizni avtomatlashtiring.",
+  subheadline:
+    "Onlayn biznesdagi muvaffaqiyatning yangi namunasiga aylaning. Hammasi bitta platformada.",
   mainCTA: {
-    content: 'try it for free',
-    href: '#'
+    content: "Bepul sinab ko'rish",
+    href: "https://app.ezzyshop.uz",
   },
   secondaryCTA: {
-    content: 'view live demo',
-    href: '#'
+    content: "Demo do'kon",
+    href: "https://t.me/ezzyshopuz_bot",
   },
-  illustration: '/illustration.svg',
+  illustration: "/illustration.png",
   reviews: [
     {
       name: "John Doe",
@@ -52,53 +53,72 @@ const settings = {
       designation: "Soap Developer",
       image:
         "https://images.unsplash.com/photo-1472099645785-5658abf4ff4e?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=3540&q=80",
-    }
-  ]
-}
+    },
+  ],
+};
 
 export default function Hero() {
   return (
     <div className="space-y-12 md:space-y-20 lg:space-y-28 z-50 relative">
       <Navbar />
 
-      <section className="flex flex-col gap-8 lg:gap-11 items-center text-center">
+      <section className="flex flex-col gap-6 lg:gap-8 items-center text-center">
         {/* Headline */}
-        <h1 className="text-black text-4xl md:text-6xl lg:text-hero font-medium tracking-tight leading-none xl:max-w-3/4">
-          <TextBlurEffect className='text-transparent bg-clip-text bg-gradient-to-b from-black to-black/60'>{settings.headline}</TextBlurEffect>
+        <h1 className="text-black text-4xl md:text-5xl lg:text-hero whitespace-break-spaces font-medium tracking-tight leading-none xl:max-w-3/4">
+          <TextBlurEffect className="text-transparent bg-clip-text bg-gradient-to-b from-black to-black/60">
+            {settings.headline}
+          </TextBlurEffect>
         </h1>
 
         {/* Sub-headline */}
         <SlideEffect
           delay={0}
-          className="text-sm lg:text-base px-6 sm:px-10 md:px-0 md:max-w-3/4 mx-auto"
+          className="text-sm lg:text-base px-6 sm:px-10 md:px-0 md:max-w-3/5 mx-auto"
         >
           {settings.subheadline}
         </SlideEffect>
 
         {/* CTA */}
-        <SlideEffect
-          className="flex flex-col gap-8 md:gap-5 items-center justify-center w-full md:w-fit"
-        >
+        <SlideEffect className="flex flex-col gap-8 md:gap-5 items-center justify-center w-full md:w-fit">
           {/* Buttons */}
           <div className="flex flex-col md:flex-row items-center w-full justify-center gap-3 md:gap-4 mt-1">
-            <Link href={settings.mainCTA.href} className="w-full">
-              <Button size='lg' className="capitalize w-full hidden lg:flex">
+            <Link
+              href={settings.mainCTA.href}
+              target="_blank"
+              className="w-full"
+            >
+              <Button size="lg" className="capitalize w-full hidden lg:flex">
                 {settings.mainCTA.content}
                 <ArrowRight />
               </Button>
 
-              <Button size='default' className="capitalize w-full flex lg:hidden">
+              <Button
+                size="default"
+                className="capitalize w-full flex lg:hidden"
+              >
                 {settings.mainCTA.content}
                 <ArrowRight />
               </Button>
             </Link>
 
-            <Link href={settings.secondaryCTA.href} className="w-full">
-              <Button size='lg' className="capitalize w-full hidden lg:flex" variant='secondary'>
+            <Link
+              href={settings.secondaryCTA.href}
+              target="_blank"
+              className="w-full"
+            >
+              <Button
+                size="lg"
+                className="capitalize w-full hidden lg:flex"
+                variant="secondary"
+              >
                 {settings.secondaryCTA.content}
               </Button>
 
-              <Button size='default' className="capitalize w-full flex lg:hidden" variant='secondary'>
+              <Button
+                size="default"
+                className="capitalize w-full flex lg:hidden"
+                variant="secondary"
+              >
                 {settings.secondaryCTA.content}
               </Button>
             </Link>
@@ -110,11 +130,31 @@ export default function Hero() {
 
             <div className="flex flex-col justify-center items-center md:items-start gap-1 md:gap-2">
               <div className="flex gap-px">
-                <Star size={16} fill='oklch(0.795 0.184 86.047)' className="text-yellow-500" />
-                <Star size={16} fill='oklch(0.795 0.184 86.047)' className="text-yellow-500" />
-                <Star size={16} fill='oklch(0.795 0.184 86.047)' className="text-yellow-500" />
-                <Star size={16} fill='oklch(0.795 0.184 86.047)' className="text-yellow-500" />
-                <Star size={16} fill='oklch(0.795 0.184 86.047)' className="text-yellow-500" />
+                <Star
+                  size={16}
+                  fill="oklch(0.795 0.184 86.047)"
+                  className="text-yellow-500"
+                />
+                <Star
+                  size={16}
+                  fill="oklch(0.795 0.184 86.047)"
+                  className="text-yellow-500"
+                />
+                <Star
+                  size={16}
+                  fill="oklch(0.795 0.184 86.047)"
+                  className="text-yellow-500"
+                />
+                <Star
+                  size={16}
+                  fill="oklch(0.795 0.184 86.047)"
+                  className="text-yellow-500"
+                />
+                <Star
+                  size={16}
+                  fill="oklch(0.795 0.184 86.047)"
+                  className="text-yellow-500"
+                />
               </div>
 
               <span className="text-xs">Trusted by 500+ Customers</span>
@@ -126,7 +166,7 @@ export default function Hero() {
         <SlideEffect className="relative" isSpring={false} duration={1.3}>
           <Image
             src={settings.illustration}
-            alt='illustration'
+            alt="illustration"
             width={1920}
             height={1080}
             priority
@@ -136,5 +176,5 @@ export default function Hero() {
         </SlideEffect>
       </section>
     </div>
-  )
+  );
 }
